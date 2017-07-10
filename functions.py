1e627e248dc7ae5f67465f69da7a59176d5c848c
Neko4me <linyc47@163.com>
2017-07-10 17:30:15 +0800

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from bullet import Bullet
+from alien import Alien
 def chekc_keydown_events(event,ai_setting,scree,ship,bullets):
 	if event.key==pygame.K_SPACE:
 		if len(bullets)<ai_setting.bullet_allow:
@@ -25,11 +26,12 @@ def check_events(ship,ai_setting,scree,bullets):
 				ship.moving_left=False
 				
 
-def update_screen(ai_setting,scree,ship,bullets):
+def update_screen(ai_setting,scree,ship,bullets,aliens):
 	scree.fill(ai_setting.bg_color)
 	for bullet in bullets.sprites():
 		bullet.draw_bullet()
 	ship.bitme()
+	aliens.draw(scree)
 	pygame.display.flip()
 
 def update_bullet(bullets):
@@ -37,3 +39,30 @@ def update_bullet(bullets):
 	for bullet in bullets.copy():
 		if bullet.rect.bottom<=0:
 			bullets.remove(bullet)
+
+def get_number_row(ai_setting,ship_height,alien_height):
+	available_space_y=(ai_setting.screen_hight-(2*alien_height)-ship_height)
+	number_rows=int(available_space_y/(3*alien_height))
+	return number_rows
+
+def get_number_alien_x(ai_setting,alien_width):
+	available_space_x=ai_setting.screen_width -1.5*alien_width
+	number_aliens_x=int(available_space_x/(1.5*alien_width))
+	return number_aliens_x
+
+def create_alien(ai_setting,scree,aliens,alien_number,row_number):
+	alien=Alien(ai_setting,scree)
+	alien_width=alien.rect.width
+	alien.x=alien_width+1.5*alien_width*alien_number
+	alien.rect.x=alien.x
+	alien.rect.y=alien.rect.height+2*alien.rect.height*row_number
+	aliens.add(alien)
+
+def create_fleet(ai_setting,scree,ship,aliens):
+	alien=Alien(ai_setting,scree)
+	number_aliens_x=get_number_alien_x(ai_setting,alien.rect.width)
+	number_rows=get_number_row(ai_setting,ship.rect.height,alien.rect.height)
+	for row_number in range(number_rows):
+		for alien_number in range(number_aliens_x):
+			create_alien(ai_setting,scree,aliens,alien_number,row_number)
+			
--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import pygame
 from pygame.sprite import Group
 from setting import setting
 from ship import Ship
+from alien import Alien
 import functions as gf
 
 def run_game():
 	pygame.init()
 	ai_setting=setting()
 	scree=pygame.display.set_mode((ai_setting.screen_width,ai_setting.screen_hight))
+	aliens=Group()
 	ship=Ship(scree,ai_setting)
+	alien=Alien(ai_setting,scree)
+	gf.create_fleet(ai_setting,scree,ship,aliens)
 	bullets=Group()
 	pygame.display.set_caption('New game')
 	
@@ -18,6 +22,5 @@ def run_game():
 		gf.check_events(ship,ai_setting,scree,bullets)
 		ship.update()
 		gf.update_bullet(bullets)
-		gf.update_screen(ai_setting,scree,ship,bullets)
-		
+		gf.update_screen(ai_setting,scree,ship,bullets,aliens)
 run_game()
